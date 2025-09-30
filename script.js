63affc5f442fe8b1b4c44f92786bb8a7283e0561
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-09-30 15:31:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
   // Check if the body has any child elements. A length of 0 confirms it is empty.
   
     document.body.append("DOM load success");
+	const scriptElement = document.getElementById("script");
+    if (scriptElement) {
+        scriptElement.remove();
+    }
   
 });

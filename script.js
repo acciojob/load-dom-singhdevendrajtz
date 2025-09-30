d5e53877e2aa09fff9f694d6d3a20e6b663097e8
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-09-30 14:58:06 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-//your JS code here. If required.
-document.addEventListener('DOMContentLoaded',function(){
-//const el1=document.createElement("h1");
-let el1= "DOM load success";
-document.body.append(el1)
-})
